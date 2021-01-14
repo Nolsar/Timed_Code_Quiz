32322abe9428e595d01819e1b61582146e4bb7f0
Sara Nolan <saraa.ann28@gmail.com>
2021-01-13 21:18:27 -0500

    added timer when start button is clicked

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 // test to check that script is working
 // console.log("test");
 
+//Declared variables
+var startButton = document.querySelector("#startButton");
+var timer = document.querySelector("#timer");
+var timeInterval = 0;
+var secondsLeft = 75
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
-// THEN a timer starts and I am presented with a question
+startButton.addEventListener("click", function(){
+
+    // THEN a timer starts
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function(){
+            secondsLeft--;
+            timer.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <=0) {
+                clearInterval(timeInterval);
+                finsihed();
+                timer.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+})
+
+function finished(){
+    alert("DONE");
+}
+
+
+    // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
